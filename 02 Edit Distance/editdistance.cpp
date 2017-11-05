028c2d609bce42c3e6a0b9f52fd1715506346e19
Harish <hthuwal@gmail.com>
2017-11-05 14:37:04 +0530

    Add edit distance function
    Gives Segmentation fault for large inputs

--- 02 Edit Distance/editdistance.cpp
+++ 02 Edit Distance/editdistance.cpp
@@ -38,6 +38,50 @@ void read(char del)
 	del_cost = atol(temp.c_str());
 }
 
+lli editdistance_dp()
+{
+	lli n = x.length();  /* Length of string x*/
+	lli m = y.length();  /* Length of string y*/
+
+	/*Dynamic Programming table*/
+	lli dp[n+1][m+1];
+	/*dp[i][j] denotes the minimum cost in transforming i characters of x (i.e x[0:i-1]) to j characters of y (i.e y[0:j-1])*/
+
+	/*if x is an empty string then only option is to insert all the elements of y*/
+	for(lli i=0;i<=m;i++)
+		dp[0][i] = ((i%M)*(in_cost%M)%M);
+
+	/*if y is an empty string then only option is to delete all the elements of x*/
+	for(lli i=0;i<=n;i++)
+		dp[i][0] = ((i%M)*(del_cost%M)%M);
+
+	for(lli i = 1 ; i <= n ; i++)
+	{
+		for(lli j = 1 ; j <= m ; j++)
+		{
+			/* try insert */
+			lli a = (in_cost % M + dp[i][j-1])%M;
+
+			/* try replace */
+			lli b = (re_cost % M + dp[i-1][j-1])%M;
+
+			/* try del */
+			lli c = (del_cost % M + dp[i-1][j])%M;
+
+			dp[i][j] = a < b ? a : b;
+			dp[i][j] = dp[i][j] < c ? dp[i][j] : c;
+
+			/*try copy*/
+			if(x[i-1]==y[j-1])
+			{
+				lli d = (cp_cost % M + dp[i-1][j-1])%M;
+				dp[i][j] = dp[i][j] < d ? dp[i][j] : d;
+			}
+		}
+	}
+	return dp[n][m];
+}
+
 int main(int argc, char *argv[])
 {
 	if(argc != 2)
@@ -61,5 +105,6 @@ int main(int argc, char *argv[])
 	{
 		cout<<"t is "<<t<<endl;
 		read(',');
+		cout<<editdistance_dp()<<endl;
 	}
 }
